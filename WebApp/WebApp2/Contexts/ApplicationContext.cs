738f171dcac0047e208887c4a51e2cb2d09d211b
Christopher Kadlec <chris.kadlec@hotmail.com>
2021-10-09 18:40:33 +0200

    add existing project with sql database

---
+++ WebApp/WebApp2/Contexts/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Models;
+
+namespace WebApp2.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
