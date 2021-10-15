c6d9d5cf26b10e898225086381e780baf9fdd1c9
Christopher Kadlec <chris.kadlec@hotmail.com>
2021-10-15 14:42:35 +0200

    add Services and Interfaces for token and user

---
+++ WebApp/WebApp2/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Models;
+
+namespace WebApp2.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateJwtToken(User user);
+        long GetUserId(string token);
+    }
+}
---
+++ WebApp/WebApp2/Services/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Models;
+using WebApp2.Models.DTOs;
+
+namespace WebApp2.Services.Interfaces
+{
+    public interface IUserService
+    {
+        User AddUser(string email, string password);
+        User FindByEmail(string email);
+        bool ValidPassword(string password);
+        string Register(UserRequestDTO userRequestDTO);
+        string Login(UserRequestDTO userRequestDTO);
+        User FindById(long id);
+    }
+}
