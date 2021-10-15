2088b7d67412546cf1fecb5b34242ac2b5a90039
Christopher Kadlec <chris.kadlec@hotmail.com>
2021-10-15 14:43:54 +0200

    add Master, User and temporary TestAuth controllers

---
+++ WebApp/WebApp2/Controllers/MasterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2.Controllers
+{
+    public abstract class MasterController : ControllerBase
+    {
+        public long userId { get; set; }
+
+        public MasterController(IHttpContextAccessor httpContext)
+        {
+            userId = (long)httpContext.HttpContext.Items["UserId"];
+        }
+    }
+}
