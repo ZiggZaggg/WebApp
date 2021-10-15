5b0d7cc2ef027b6545bc97508a45f07399d5bc8b
Christopher Kadlec <chris.kadlec@hotmail.com>
2021-10-15 18:13:34 +0200

    add JWTMiddleware intergration test

--- WebApp/WebAppIntegrationTest/IntegrationTests/JwtMiddlewareIntegrationTest.cs
+++ WebApp/WebAppIntegrationTest/IntegrationTests/JwtMiddlewareIntegrationTest.cs
@@ -1,17 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp2.Helpers;
+using WebApp2.Models;
+using WebApp2.Services;
 using Xunit;
 
 namespace WebAppIntegrationTest
 {
-    class JwtMiddlewareIntegrationTest : TestBuilder, IClassFixture<CustomWebApplicationFactory>
+    public class JwtMiddlewareIntegrationTest : TestBuilder, IClassFixture<CustomWebApplicationFactory>
     {
         public JwtMiddlewareIntegrationTest(CustomWebApplicationFactory factory)
         {
             ConfigureTest(factory, services => { });
+        }
+
+        [Fact]
+        public void AttachUserToContext_OK()
+        {
+            var httpContextMoq = new Mock<HttpContext>();
+            httpContextMoq.Setup(c => c.Items).Returns(new Dictionary<object, object>());
+            var nextMoq = new Mock<RequestDelegate>();
+            var configMoq = new Mock<IConfiguration>();
+            configMoq.Setup(c => c.GetValue(typeof(string), "TokenKey")).Returns(config["TokenKey"]);
+            var tokenService = new TokenService(config);
+            var token = tokenService.GenerateJwtToken(new User { Id = 1 });
+            var jwtMiddleware = new JwtMiddleware(nextMoq.Object, configMoq.Object);
+
+            jwtMiddleware.AttachUserToContext(httpContextMoq.Object, token);
+
+
         }
     }
 }
